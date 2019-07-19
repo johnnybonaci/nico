458ecd5531ca5fa045d2e122013dbbc8fc1e4452
jbonaci <johnnybonaci@gmail.com>
2019-07-19 17:07:31 -0300

    arreglo de datos

--- nico.php
+++ nico.php
@@ -24,6 +24,38 @@ $headers = array(
 	    return $src;
 	}
 
+	function extraerArraysVacios($array){
+
+		$array_4 = array_filter(array_map('trim', $array));
+        $array_5 = array_values(($array_4));
+        return $array_5;
+
+	}
+
+	function parsearBloques($string,$variables){
+
+		$parse_0 = str_replace($variables[0], $variables[1], $string);
+        $parse_1 = str_replace($variables[1], "<$>", $parse_0);
+        $parse_2 = str_replace($variables[2], "", $parse_1);
+        $parse_3 = str_replace($variables[3], "", $parse_2);
+        $parse_4 = explode("<$>", $parse_3);
+        return $parse_4;
+
+	}
+
+	function unirTextos($texto){
+
+		$texto = array_values($texto);
+		$parrafo="";
+		foreach ($texto as $key => $value) {
+			
+			$parrafo .= '<p>'.$value.'</p>';
+		}
+
+		return $parrafo;
+
+	}
+
 $data = new RestWs($url, $headers);
         try {
 
@@ -33,62 +65,51 @@ $data = new RestWs($url, $headers);
 
         	var_dump($e->getMessage());
 		}
-
+		// Separa el contenido en bloques
         $proData = json_decode($data->getResult(),true);
-
         $content = $proData['content']["rendered"];
-
-        ///Variables////
         $noticias = explode("<!--nextpage-->", $content);
-        $replace_h2 = array('<h2 style="text-align: center;">', '</h2>');
-        $replace = array('<em>', '<div id="containerMrec"></div>');
+        ///Variables////
+        $replace_h2 = array('<h2 style="text-align: center;">','</h2>');
+        $replace = array('<div id="containerMrec"></div>','</body></html>');
+        $replace_em = array('<blockquote>', '</blockquote>');
+        $replace_p = array('<p>', '</p>');
+        $variables = array($replace,$replace_p,$replace_em,$replace_h2);
         $total_n = count($noticias);
         $ultimo = $total_n - 1;
+
         //// Proceso para clasificar el contenido///
         foreach ($noticias as $key => $value) {
 
-	        $parse_1 = str_replace("</p>", "<$>", $value);
-	        $parse_2 = str_replace("<p>", "<$>", $parse_1);
-	        $parse_3 = explode("<$>", $parse_2);
-	        if($key === 0){
+	        $bloque = explode("<img class", $value);
 
-	        	unset($parse_3[0]);
-		        $parse_3 = explode("<img", $parse_3[1]);
-		        $parse_3[0]= str_replace($replace, '', $parse_3[0]);
-		        $parse_3[1] = $parse_3[1];
-		        $parse_3[1] = str_replace('</em>', '', $parse_3[1]);
+        	//Primer bloque Titulo Descripcion///
+        	$bloque_1 = parsearBloques($bloque[0], $variables);
+        	$bloque_2 = extraerArraysVacios($bloque_1);
+        	$datos['name'] = ($key == 0) ? "" : $bloque_2[0];unset($bloque_2[0]);
+        	$datos['description'] = unirTextos($bloque_2);
+        	///Segundo Bloque img credit
+        	$bloque_3 = parsearBloques($bloque[1], $variables);
+        	$bloque_4 = extraerArraysVacios($bloque_3);
+        	$datos['img'] = extraerSRC($bloque_4[0]);unset($bloque_4[0]);
+        	$datos['above_text'] = "";
+			$datos['attribution'] = "";
+			$datos['credit'] = "";
+			$datos['text'] = "";
+        	$bloque_4 = array_values($bloque_4);
+
+        	if(count($bloque_4) > 0){
+
+	        	$datos['credit'] = $bloque_4[0];unset($bloque_4[0]);
+	        	$bloque_4 = array_values($bloque_4);
+	        	$datos['text'] = unirTextos($bloque_4);
 	        }
-	        if ($key === $ultimo) {
-	        	unset($parse_3[count($parse_3)-1]);
-	        }
 
-	        $parse_4 = array_filter(array_map('trim', $parse_3));
-	        $parse_5 = array_values(($parse_4));
-        	$notice[] = $parse_5;
-        }
-
-
-//////////// Armado de json//////////////
-
-	foreach ($notice as $key => $value) {
-			
-		$datos['name'] = ($key == 0) ? "" : str_replace($replace_h2, "", $value[0]);
-		$datos['description'] = ($key == 0) ? str_replace($replace_h2, "", $value[0]) : str_replace($replace_h2, "", $value[1]);
-		$datos['img'] = ($key == 0) ? extraerSRC($value[1]) : extraerSRC($value[2]);
-		$datos['credit'] = ($key == 0) ? "" : $value[3];
-		$datos['text'] = ($key == 0) ? "" : $value[4];
-		$datos['above_text'] = "";
-		$datos['attribution'] = "";
-
-		$json[] = $datos;
-
-
-	}
-
-	//$json = str_replace("\u0022","\\\\\"",json_encode( $json,JSON_HEX_QUOT));
+    		$notice[] = $datos;
+	    }
 
     header('Content-type: application/json; charset=utf-8');
-	echo json_encode($json);
+	echo json_encode($notice);
 
 	 exit();
 
